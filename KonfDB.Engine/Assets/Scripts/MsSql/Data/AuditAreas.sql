5d4c9fd6693239f821a037c2a20364f13a5cc0ac
Punit Ganshani <punit.ganshani@gmail.com>
2014-09-28 23:47:05 +0800

    Spell mistake
    spell mistak

--- KonfDB.Engine/Assets/Scripts/MsSql/Data/AuditAreas.sql
+++ KonfDB.Engine/Assets/Scripts/MsSql/Data/AuditAreas.sql
@@ -1,7 +1,7 @@
 ﻿IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=100) INSERT INTO [Config].[AuditArea] VALUES (100, 'Access') ELSE UPDATE  [Config].[AuditArea] SET AuditAreaName='Access' WHERE AuditAreaId=100
 IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=200) INSERT INTO [Config].[AuditArea] VALUES (200, 'Application') ELSE UPDATE   [Config].[AuditArea] SET AuditAreaName='Application' WHERE AuditAreaId=200
 IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=300) INSERT INTO [Config].[AuditArea] VALUES (300, 'Environment') ELSE UPDATE  [Config].[AuditArea] SET AuditAreaName='Environment' WHERE AuditAreaId=300
-IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=400) INSERT INTO [Config].[AuditArea] VALUES (400, 'Mappint') ELSE UPDATE  [Config].[AuditArea] SET AuditAreaName='Mapping' WHERE AuditAreaId=400
+IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=400) INSERT INTO [Config].[AuditArea] VALUES (400, 'Mapping') ELSE UPDATE  [Config].[AuditArea] SET AuditAreaName='Mapping' WHERE AuditAreaId=400
 IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=500) INSERT INTO [Config].[AuditArea] VALUES (500, 'Parameter') ELSE UPDATE [Config].[AuditArea] SET AuditAreaName='Parameter' WHERE AuditAreaId=500
 IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=600) INSERT INTO [Config].[AuditArea] VALUES (600, 'Region') ELSE UPDATE [Config].[AuditArea] SET AuditAreaName='Region' WHERE AuditAreaId=600
 IF NOT EXISTS(SELECT * FROM [Config].[AuditArea] WHERE AuditAreaId=700) INSERT INTO [Config].[AuditArea] VALUES (700, 'Server') ELSE UPDATE [Config].[AuditArea] SET AuditAreaName='Server' WHERE AuditAreaId=700
