60d7a70d0dec5eadef750e7d73a58c0e7e0e0b0f
Punit Ganshani <punit.ganshani@gmail.com>
2014-10-17 00:01:12 +0800

    KonfDBCF and KonfDBRC
    Removed dependency of KonfDBI from both .NET client and Remote Client

--- KonfDB.Engine/Properties/AssemblyInfo.cs
+++ KonfDB.Engine/Properties/AssemblyInfo.cs
@@ -72,5 +72,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
