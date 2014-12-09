e37e42ba4d780e4fb00a15b9a616f601068cba9f
Punit Ganshani <punit.ganshani@gmail.com>
2014-12-09 10:46:47 +0800

    Default Configuration
    Default Configuration

--- buildall.bat
+++ buildall.bat
@@ -2,4 +2,8 @@ REM Build KonfDB
 SET CONFIGURATION=%1
 set PATH_SOURCE_SLN="%cd%\KonfDB.sln"
 
+if [%1]==[] (
+  SET CONFIGURATION=debug-net4.0
+)
+
 MSBuild %PATH_SOURCE_SLN% /p:Configuration=%CONFIGURATION%
